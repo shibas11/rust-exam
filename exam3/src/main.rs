a337c230cce42b2a5cac1a7ec43a519a068fba1b
shibas11 <shibas11@gmail.com>
2026-01-28 16:08:38 +0900

    error exam for trait

--- exam3/src/main.rs
+++ exam3/src/main.rs
@@ -5,10 +5,10 @@ fn main() {
         active: true,
     };
 
-    meet(&cat, &gildong);
+    meet(&cat, &gildong); // 에러. Person이 Greet을 구현한 것은 맞지만, T타입이 서로 같아야 하는데 Pet과 Person으로 다르기 때문
 }
 
-fn meet(one: &impl Greet, another: &impl Greet) {
+fn meet<T: Greet>(one: &T, another: &T) {
     println!("첫번째가 인사합니다 {}", one.greeting());
     println!("두번째가 인사합니다 {}", another.greeting());
 }
