419a1af49af18135b651cd19d7678d52b0ff9feb
shibas11 <shibas11@gmail.com>
2026-01-28 15:51:10 +0900

    generic

--- exam2/src/main.rs
+++ exam2/src/main.rs
@@ -1,28 +1,16 @@
 fn main() {
     let numbers = vec![3, 4, 1, 6, 8, 10];
 
-    let result = smallest_i32(&numbers);
+    let result = smallest(&numbers);
     println!("가장 작은 수는 {}", result);
 
     let chars = vec!['홍', '길', '동'];
 
-    let result = smallest_char(&chars);
+    let result = smallest(&chars);
     println!("가장 작은 글자는 {}", result);
 }
 
-fn smallest_i32(list: &[i32]) -> &i32 {
-    let mut smallest = &list[0];
-
-    for item in list {
-        if item < smallest {
-            smallest = item;
-        }
-    }
-
-    smallest
-}
-
-fn smallest_char(list: &[char]) -> &char {
+fn smallest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut smallest = &list[0];
 
     for item in list {
